c7cb1a3b417b3613e21ab64559cb4993b48e6d34
Yann Duval <yann.duval@valorissimo.com>
2022-08-08 18:15:59 +0200

    layout signature

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,11 +10,29 @@ module.exports = {
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
-      colors: {
-        // Other colors
-      },
       fontFamily: {
       },
+      boxShadow: {
+        'custom': '0 2px 24px rgb(0 0 0 / 12%)',
+      },
+      colors: {
+        'orange-valo': '#fe7443',
+        'orange-valo-light': '#fe9c42',
+        'cyan-light': '#A8FFFA',
+        'cyan': '#77E5F5',
+        'grey-1': '#1e1e1e'
+      },
+      fontSize: {
+        'xxs': '.65rem',
+        '4xl': '2.5rem',
+        '5xl': '3.5rem',
+        '6xl': '5rem',
+      },
+      spacing: {
+        '18': '4.5rem',
+        '22': '5.5rem',
+        '88': '22rem',
+      }
     },
   },
   variants: {
